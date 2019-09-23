bf739a00b208821fc18dab92ee86f28c9f1110d4
Dat Tran <dtran@protectmycar.com>
2019-09-22 20:59:52 -0400

    Clientside mvp working. Starting middleware.

---
+++ HackerNews/wwwroot/js/main.js
@@ -0,0 +1,66 @@
+﻿var app = app || {
+    addStory: function (id) {
+        let self = this;
+        let storyContainer = document.getElementById('stories-container');
+
+        let story = document.createElement('div');
+        story.setAttribute('id', `story-${id}`)
+        story.innerHTML = 'loading story...';
+        storyContainer.appendChild(story);
+
+        self.getStoryById(id);
+    },
+    updateStory: function (id, title, author, url) {
+        let self = this;
+
+        let story = document.getElementById(`story-${id}`);
+        story.innerHTML = self.template(title, author, url);
+    },
+    clearStories: function () {
+        let element = document.getElementById("stories-container");
+        element.innerHTML = '';
+    },
+    template: (title, author, url) => {
+        return `<div><h2><a href="${url}"> ${title}</a><h2></div><div>by ${author}</div>`;
+    },
+    getStoryById: function (id) {
+        let self = this;
+        let success = function (e) {
+            if (e.target.response) {
+                const story = JSON.parse(e.target.response);
+                self.updateStory(story.id, story.title, story.by, story.url);
+            }
+        };
+        let error = function (e) {
+            console.log('request failed. Ex:');
+            console.log(e);
+        };
+
+        service.request('GET', `https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(success, error);
+    },
+    getNewStories: function () {
+        let self = this;
+        let success = function (e) {
+            //pre-render the div's while loading
+            if (e.target.response) {
+                const storyIdArray = JSON.parse(e.target.response);
+                storyIdArray.forEach(id => {
+                    self.addStory(id);
+                    self.story
+                });
+
+
+            }
+        };
+        let error = function (e) {
+            console.log('request failed. Ex:');
+            console.log(e);
+        };
+
+        service.request('GET', 'https://hacker-news.firebaseio.com/v0/newstories.json').then(success, error);
+    },
+}
+
+window.addEventListener('load', (event) => {
+    app.getNewStories();
+});
---
+++ HackerNews/wwwroot/js/service.js
@@ -0,0 +1,15 @@
+﻿var service = service || {
+    //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+    //Wrapper function for calls 
+    request: (verb, url) => {
+        return new Promise(function(resolve, reject){
+            let xhr = new XMLHttpRequest();
+
+            xhr.onload = resolve;
+            xhr.onerror = reject;
+
+            xhr.open(verb, url);
+            xhr.send();
+        });
+    },
+}
